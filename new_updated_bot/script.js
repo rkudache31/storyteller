b4f6a28892f109b6be0f96f3f683729b65936860
SaniyaShaikh24 <saniyashaikh2462005@gmail.com>
2026-02-23 14:49:14 +0530

    Add files via upload

---
+++ new_updated_bot/script.js
@@ -0,0 +1,34 @@
+function downloadCoverLetter() {
+  window.print();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("openChatBtn");
+  const wrapper = document.getElementById("chatWrapper");
+  const frame = document.getElementById("chatFrame");
+  const chatbot = document.getElementById("chatbot");
+
+  if (!btn || !wrapper || !frame || !chatbot) {
+    console.error("Chatbot elements not found");
+    return;
+  }
+
+  btn.addEventListener("click", () => {
+
+    // load iframe only once
+    if (!frame.src) {
+      frame.src = "https://rkudache31-rkudache31career-conversation-2026-updated.hf.space/?__theme=light";
+    }
+
+    // show chatbot
+    wrapper.classList.remove("hidden");
+
+    // scroll AFTER it becomes visible
+    setTimeout(() => {
+      window.scrollTo({
+        top: chatbot.offsetTop - 20,
+        behavior: "smooth"
+      });
+    }, 150);
+  });
+});
